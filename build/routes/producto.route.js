5132c1e576b14384b4098dadcb27f21e9c799053
Juan Jose Figueroa <jjfigueroah.desarrollador@gmail.com>
2021-12-19 11:00:59 -0500

    Creacion de ruta para obtener todos los productos

--- build/routes/producto.route.js
+++ build/routes/producto.route.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const productoRoute = express_1.Router();
+productoRoute.post('/create');
 exports.default = productoRoute;
 //# sourceMappingURL=producto.route.js.map
--- src/routes/producto.route.ts
+++ src/routes/producto.route.ts
@@ -55,4 +55,42 @@ productoRoute.post(
   '/create'
 );
 
+/**
+ * @swagger
+ * /producto:
+ *  get:
+ *    description: "Obtener todos los productos"
+ *    tags:
+ *      - Producto
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *       200:
+ *         examples:
+ *           application/json: {
+ *              "_id": 606c8c02a4179839540ab167,
+ *              "nombre": "Lapiz borrable",
+ *              "descripcion": "Lapiz para niños",
+ *              "precio": "1200",
+ *              "municipio": 606c8c24a4179839540ab168,
+ *              "created_at": "2021-12-19T01:07:31.210Z",
+ *              "updated_at": "2021-12-19T01:10:30.126Z"
+ *           }
+ *       404:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             errors:
+ *               type: object
+ *         examples:
+ *           application/json: {
+ *             "errors": [
+ *                  "message": "Not Found"
+ *              ],
+ *           }
+ */
+productoRoute.get(
+  '/'
+);
+
 export default productoRoute;
