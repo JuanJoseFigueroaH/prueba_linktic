535c05b6588defca0bb61ec4173259e1193b6322
Juan Jose Figueroa <jjfigueroah.desarrollador@gmail.com>
2021-12-19 11:09:21 -0500

    Creacion de ruta para la modificacion de producto

--- build/routes/producto.route.js
+++ build/routes/producto.route.js
@@ -3,5 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const productoRoute = express_1.Router();
 productoRoute.post('/create');
+productoRoute.get('/');
+productoRoute.get('/filter/name-category');
 exports.default = productoRoute;
 //# sourceMappingURL=producto.route.js.map
--- src/routes/producto.route.ts
+++ src/routes/producto.route.ts
@@ -93,4 +93,106 @@ productoRoute.get(
   '/'
 );
 
+/**
+ * @swagger
+ * /producto/filter/name-category/{philter}:
+ *  get:
+ *    description: "Obtener productos filtrados por nombre o categoria"
+ *    tags:
+ *      - Producto
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: philter
+ *        in: path
+ *        required: true
+ *        type: string
+ *    responses:
+ *       200:
+ *         examples:
+ *           application/json: {
+ *              "_id": 606c8c02a4179839540ab167,
+ *              "nombre": "Lapiz borrable",
+ *              "descripcion": "Lapiz para niños",
+ *              "precio": "1200",
+ *              "municipio": 606c8c24a4179839540ab168,
+ *              "created_at": "2021-12-19T01:07:31.210Z",
+ *              "updated_at": "2021-12-19T01:10:30.126Z"
+ *           }
+ *       404:
+ *         schema:
+ *           type: object
+ *           properties:
+ *             errors:
+ *               type: object
+ *         examples:
+ *           application/json: {
+ *             "errors": [
+ *                  "message": "Not Found"
+ *              ],
+ *           }
+ */
+productoRoute.get(
+  '/filter/name-category/:philter'
+);
+
+/**
+ * @swagger
+ * /producto/modify/{id}:
+ *  put:
+ *    description: "Modificación de un producto"
+ *    tags:
+ *      - Producto
+ *    produces:
+ *      - application/json
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        type: number
+ *      - name: titulo
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: descripcion
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: precio
+ *        in: formData
+ *        required: true
+ *        type: string
+ *      - name: categoria
+ *        in: formData
+ *        type: ObjectId
+ *    responses:
+ *       201:
+ *         examples:
+ *           application/json: {
+ *              "_id": 606c8c02a4179839540ab167,
+ *              "nombre": "Lapiz borrable",
+ *              "descripcion": "Lapiz para niños",
+ *              "precio": "1200",
+ *              "municipio": 606c8c24a4179839540ab168,
+ *              "created_at": "2021-12-19T01:07:31.210Z",
+ *              "updated_at": "2021-12-19T01:10:30.126Z"
+ *           }
+ *       400:
+ *         description: bad request
+ *         examples:
+ *           application/json: {
+ *            "errors": [
+ *                {
+ *                    "message": "Name is required",
+ *                    "field": "name"
+ *                }
+ *            ]
+ *           }
+ */
+productoRoute.put(
+  'modify/:id'
+);
+
 export default productoRoute;
